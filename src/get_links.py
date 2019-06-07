199e3f77e78357c7e632669734219b6afd2956db
km <km@localhost.com>
2019-06-07 12:00:40 +0530

    Refactored code into a new Function get_links. Returns a list of links

--- src/get_links.py
+++ src/get_links.py
@@ -10,8 +10,14 @@ bsobj=BeautifulSoup(html_page,bsfeature)
 """
 Get All links from Wikipedia page excluding all translated versions
 """
-for eachlink in bsobj.find_all('a'):
-    if 'href' in eachlink.attrs:
-        link=eachlink.attrs['href']
-        if 'https' in link and 'wikipedia' not in link:
-            print(link)
+def get_links():
+    link_list = []
+    for eachlink in bsobj.find_all('a'):
+        if 'href' in eachlink.attrs:
+            link = eachlink.attrs['href']
+            if 'https' in link and 'wikipedia' not in link:
+                link_list.append(str(link))
+                print(link)
+    return link_list
+
+print(get_links())
