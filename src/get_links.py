c459d04ff9ff73daebb24c05bc13f7230d1c6ae9
km <km@localhost.com>
2019-06-07 12:11:28 +0530

    Functionality changed, uses SET instead of List

--- src/get_links.py
+++ src/get_links.py
@@ -11,12 +11,12 @@ bsobj=BeautifulSoup(html_page,bsfeature)
 Get All links from Wikipedia page excluding all translated versions
 """
 def get_links():
-    link_list = []
+    link_list = set()
     for eachlink in bsobj.find_all('a'):
         if 'href' in eachlink.attrs:
             link = eachlink.attrs['href']
             if 'https' in link and 'wikipedia' not in link:
-                link_list.append(str(link))
+                link_list.add(str(link))
                 print(link)
     return link_list
 
